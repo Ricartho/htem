ca92813c5e8a74323b64d430d9d68f56d3428631
Ricartho <ricartho.franck@esih.edu>
2019-05-09 17:05:42 -0400

    second commit

---
+++ lib/htem/cli.rb
@@ -0,0 +1,15 @@
+class Htem::Cli
+
+  def run
+    list_article
+  end 
+
+  def list_article
+  	puts "Welcome to Htem :"
+  	puts <<-DOC.gsub /^\s*/, ''
+  		1- name - location - price
+  		2- name - location - price
+  		3- name - location - price
+  	DOC
+  end 
+end
