aa939c9551a5e13e195ad489215aabbbfac1c1fb
Ricartho Franck <ricartho.franck@esih.edu>
2019-05-12 19:50:10 +0000

    update working on Scraper

---
+++ lib/htem/scraper.rb
@@ -0,0 +1,34 @@
+require 'open-uri'
+class Htem::Scraper
+
+  # this method is responsible for scraping the index page that lists all of the articles on Htem webpage
+  def self.scrape_index_page(page_url)
+    doc = Nokogiri::HTML(open("page_url"))
+    articles = []
+    doc.css(".portfolio").each do |article|
+      title = article.css(".title-wrap h5").text
+      simple_infos = article.css(".subtitle-wrap p").text
+      url = article.css("a").attribute("href").value
+
+      article_infos = { :name => title,
+                        :infos =>simple_infos,
+                        :url_article => url
+                      }
+                      articles.push(article_infos)
+    end
+        articles
+  end
+  #end of first class method
+
+  # this method is responsible for scraping specific articles from Htem webpage
+      def self.scrape_specific_article(article_url)
+        doc = Nokogiri::HTML(open("article_url"))
+        article = {}
+        article[:name] = doc.css(".libele").text
+        article[:description] = doc.css(".description").text
+        article[:location] = doc.css(".small.location").text
+        article[:price] = doc.css(".small.price").text
+        article[:owner] = doc.css(".owner").text
+        article
+      end
+end
--- lib/htem_relatives.rb
+++ lib/htem_relatives.rb
@@ -2,6 +2,7 @@ require 'bundler/setup'
 Bundler.require(:default)
 require_relative "./htem/version"
 require_relative "./htem/cli"
+require_relative "./htem/scraper"
 
 # module Htem
 #   class Error < StandardError; end
